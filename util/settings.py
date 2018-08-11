6cf10e9ea0c18fa2d1c68c2af9caf0101566c279
Tran Sang <tranvansangk41@gmail.com>
2018-08-12 03:58:04 +0900

    change settings

--- util/settings.py
+++ util/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 class Settings:
     profile_location = os.path.join(BASE_DIR, 'profiles')
     profile_commentors_location = os.path.join(BASE_DIR, 'profiles')
-    profile_file_with_timestamp = True
-    profile_commentors_file_with_timestamp = True
-    limit_amount = 12000
+    profile_file_with_timestamp = False
+    profile_commentors_file_with_timestamp = False
+    limit_amount = 12
     scrap_posts_infos = True
     output_comments = False
     sleep_time_between_post_scroll = 1.5
