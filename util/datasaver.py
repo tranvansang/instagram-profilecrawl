d2477e3d76ad204820902065bf9714883fca31c3
Tran Sang <tranvansangk41@gmail.com>
2018-08-28 15:26:50 +0900

    don't write if failed

--- util/datasaver.py
+++ util/datasaver.py
@@ -13,9 +13,9 @@ class Datasaver:
                 "%Y-%m-%d %H-%M-%S") + '.json')
         else:
             file_profile = os.path.join(Settings.profile_location, username + '.json')
-
-        with open(file_profile, 'w') as fp:
-            fp.write(json.dumps(information, indent=4))
+        if len(information['posts']) >= Settings.limit_amount:
+            with open(file_profile, 'w') as fp:
+                fp.write(json.dumps(information, indent=4))
 
     def save_profile_commenters_txt(username, user_commented_list):
         check_folder(Settings.profile_commentors_location)
